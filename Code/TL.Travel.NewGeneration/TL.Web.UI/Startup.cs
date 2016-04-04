39f75f72347c869a1e3fe11a4d2cc7ea6368c65b
Ladleso Shaikh <ladleso.shaikh@xoriant.com>
2016-04-04 15:33:24 +0530

    empty solution.

---
+++ Code/TL.Travel.NewGeneration/TL.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TL.Web.UI.Startup))]
+namespace TL.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
