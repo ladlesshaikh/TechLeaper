e2c218393752338fb655bf567706b3a3e18909b5
Ladleso Shaikh <ladleso.shaikh@xoriant.com>
2016-04-04 19:42:15 +0530

    Added skeleton

---
+++ Code/TL.Travel.NewGeneration/TL.DAL/Base/RepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TL.DAL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RepositoryBase<T> : IRepository<T> where T:class
+    {
+        #region Local resource declarations
+        /// <summary>
+        /// 
+        /// </summary>
+        private DbContext dbContext = null;
+        #endregion
+
+        #region Object construction
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public RepositoryBase(DbContext context)
+        {
+            dbContext = context;
+        }
+        #endregion
+
+        #region IRepository<T> implimentation
+        public T Get(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Code/TL.Travel.NewGeneration/TL.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TL.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        T Get(string Id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        T Find(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        void Save(T entity);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(T entity);
+    }
+}
